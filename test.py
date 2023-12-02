4758b243a8cc043f1b0909a8d163053a435522d4
myZhang <3078270400@qq.com>
2023-12-02 17:05:36 +0800

    Add argparser to select device

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
-from thundersvm import SVC
+# from thundersvm import SVC
+# from sklearn.svm import SVC
 import torch
 import pandas as pd
+import argparse
+
 
 def prepare_data(data):
     train_x = data[data['Usage'] != 'PublicTest']
@@ -11,29 +14,43 @@ def prepare_data(data):
     test_x = test_x['pixels'].str.split(' ', expand=True).astype('float32')
     return train_x, train_y, test_x, test_y
 
-device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
-path = './data/fer2013/fer2013.csv'
-data = pd.read_csv(path)
-print("start loading data")
-train_x, train_y, test_x, test_y = prepare_data(data)
-print("loading data done")
-print(train_x.shape, train_y.shape, test_x.shape, test_y.shape)
+def main(device):
+    path = './data/fer2013/fer2013.csv'
+    data = pd.read_csv(path)
+    print("start loading data")
+    train_x, train_y, test_x, test_y = prepare_data(data)
+    print("loading data done")
+    print(train_x.shape, train_y.shape, test_x.shape, test_y.shape)
+    
+    if device == 'cpu':
+        print("Using CPU")
+        from sklearn.svm import SVC
+    else:
+        print("Using GPU")
+        from thundersvm import SVC
+        device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
+        train_x, train_y, test_x, test_y = (
+            torch.tensor(train_x.values).to(device),
+            torch.tensor(train_y.values).to(device),
+            torch.tensor(test_x.values).to(device),
+            torch.tensor(test_y.values).to(device),
+        )
+        
+    # Create SVM model on GPU
+    face = SVC(
+        C=1.0, kernel='sigmoid', gamma=0.01, cache_size=800,
+        decision_function_shape='ovr', probability=True,
+        random_state=42, verbose=1
+    )
 
-# Move data to GPU
-# train_x, train_y, test_x, test_y = (
-#     torch.tensor(train_x.values).to(device),
-#     torch.tensor(train_y.values).to(device),
-#     torch.tensor(test_x.values).to(device),
-#     torch.tensor(test_y.values).to(device),
-# )
-print(train_x.shape, train_y.shape, test_x.shape, test_y.shape)
-# Create SVM model on GPU
-face = SVC(
-    C=1.0, kernel='sigmoid', gamma=0.01, cache_size=800,
-    decision_function_shape='ovr', probability=True,
-    random_state=42, gpu_id=5, verbose=1
-)
+    # Train and test on GPU
+    face.fit(train_x, train_y)
+    print(face.score(test_x, test_y))
 
-# Train and test on GPU
-face.fit(train_x, train_y)
-print(face.score(test_x, test_y))
+if '__main__' == __name__:
+    parser = argparse.ArgumentParser(description='Description of your script')
+    parser.add_argument('-d', '--device', default='cpu', help='Specify the device (default: cpu)')
+
+    args = parser.parse_args()
+    main(args.device)
+    
