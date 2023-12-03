6c82cc7ff285c6f120c07e6281479341f4e7e242
myZhang <3078270400@qq.com>
2023-12-03 10:05:36 +0800

    Add parameters args

--- test.py
+++ test.py
@@ -12,23 +12,31 @@ def prepare_data(data):
     test_x = data[data['Usage'] == 'PublicTest']
     train_y = train_x['emotion']
     test_y = test_x['emotion']
-    # train_x = train_x['pixels'].str.split(' ', expand=True).astype('float32')
-    # test_x = test_x['pixels'].str.split(' ', expand=True).astype('float32')
-    train_x = get_hog_features(np.array(train_x['pixels'].str.split(' ', expand=True).astype('float32')))
-    test_x = get_hog_features(np.array(test_x['pixels'].str.split(' ', expand=True).astype('float32')))
+    train_x = train_x['pixels'].str.split(' ', expand=True).astype('float32')
+    test_x = test_x['pixels'].str.split(' ', expand=True).astype('float32')
     return train_x, train_y, test_x, test_y
 
-def main(device):
-    path = './data/fer2013/fer2013.csv'
+def main(args):
+    device = args.device
+    hog = args.hog
+    path = args.path
     data = pd.read_csv(path)
     print("start loading data")
     train_x, train_y, test_x, test_y = prepare_data(data)
+    if hog:
+        train_x = get_hog_features(np.array(train_x))
+        test_x = get_hog_features(np.array(test_x))
     print("loading data done")
     # print(train_x.shape, train_y.shape, test_x.shape, test_y.shape)
     
     if device == 'cpu':
         print("Using CPU")
         from sklearn.svm import SVC
+        face = SVC(
+            C=args.C, kernel=args.kernel, gamma=args.gamma, cache_size=800,
+            decision_function_shape='ovr', probability=True,
+            random_state=42, verbose=1
+        )
     else:
         print("Using GPU")
         from thundersvm import SVC
@@ -40,12 +48,12 @@ def main(device):
             torch.tensor(test_y.values).to(device),
         )
         
-    # Create SVM model on GPU
-    face = SVC(
-        C=1.0, kernel='sigmoid', gamma=0.01, cache_size=800,
-        decision_function_shape='ovr', probability=True,
-        random_state=42, verbose=1
-    )
+        # Create SVM model on GPU
+        face = SVC(
+            C=args.C, kernel=args.kernel, gamma=args.gamma, cache_size=800,
+            decision_function_shape='ovr', probability=True,
+            random_state=42, gpu_id=5, verbose=1
+        )
 
     # Train and test on GPU
     face.fit(train_x, train_y)
@@ -53,8 +61,13 @@ def main(device):
 
 if '__main__' == __name__:
     parser = argparse.ArgumentParser(description='Description of your script')
+    parser.add_argument('-p', '--path', default='./data/fer2013/fer2013.csv', help='Path to the dataset (default: ./data/fer2013/fer2013.csv)')
     parser.add_argument('-d', '--device', default='cpu', help='Specify the device (default: cpu)')
+    parser.add_argument('--hog', default=True, help='Use HOG features (default: True)')
+    parser.add_argument('--kernel', default='rbf', help='Kernel type (default: rbf)')
+    parser.add_argument('--gamma', default=0.01, help='Kernel coefficient (default: 0.01)')
+    parser.add_argument('--C', default=1.0, help='Penalty parameter C of the error term (default: 1.0)')
 
     args = parser.parse_args()
-    main(args.device)
+    main(args)
     
